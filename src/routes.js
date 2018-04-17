a25253c66c43ca77e6d9b765a42121c86ab7d927
André Felipe Ricarti <ricarti.182@gmail.com>
2018-04-17 00:21:23 -0300

    Realizado o code splitting e lazy loading.

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
-import Cadastro from './components/cadastro/Cadastro.vue';
+// import Cadastro from './components/cadastro/Cadastro.vue';
+const Cadastro = () => System.import('./components/cadastro/Cadastro.vue') 
 import Home from './components/home/Home.vue';
 
 export const routes = [
